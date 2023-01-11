d8494929ddd06c445aadececb0c7f56738a879b7
Pratik Makwana <pratik@banknovo.com>
2023-01-11 14:01:50 +0530

    updated helm-addon module

--- helm-addon/variables.tf
+++ helm-addon/variables.tf
@@ -80,3 +80,82 @@ variable "use_kubergrunt_to_fetch_token" {
   type        = bool
   default     = true
 }
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+# OPTIONAL MODULE PARAMETERS
+# These variables have defaults, but may be overridden by the operator.
+# ---------------------------------------------------------------------------------------------------------------------
+
+variable "helm_chart_version" {
+  description = "Specify the exact chart version to install. If this is not specified, the latest version is installed."
+  type        = string
+  default     = null
+}
+
+variable "helm_chart_values" {
+  description = "Map of values to pass to the Helm chart. Leave empty to use chart default values."
+  type        = any
+  default     = {}
+}
+
+variable "helm_repository" {
+  description = "Repository URL where to locate the requested chart."
+  type        = string
+}
+
+variable "helm_chart" {
+  description = "Chart name to be installed. The chart name can be local path, a URL to a chart, or the name of the chart if repository is specified. It is also possible to use the <repository>/<chart> format here if you are running Terraform on a system that the repository has been added to with helm repo add but this is not recommended."
+  type        = string
+}
+
+variable "iam_role_exists" {
+  description = "Whether or not the IAM role passed in `iam_role_name` already exists. Set to true if it exists, or false if it needs to be created. Defaults to false."
+  type        = bool
+  default     = false
+}
+
+variable "iam_policy" {
+  description = "An object defining the policy to attach to `iam_role_name` if the IAM role is going to be created. Accepts a map of objects, where the map keys are sids for IAM policy statements, and the object fields are the resources, actions, and the effect (\"Allow\" or \"Deny\") of the statement. Ignored if `iam_role_arn` is provided. Leave as null if you do not wish to use IAM role with Service Accounts."
+  type = map(object({
+    resources = list(string)
+    actions   = list(string)
+    effect    = string
+  }))
+  default = null
+
+  # Example:
+  # iam_policy = {
+  #   S3Access = {
+  #     actions = ["s3:*"]
+  #     resources = ["arn:aws:s3:::mybucket"]
+  #     effect = "Allow"
+  #   },
+  #   SecretsManagerAccess = {
+  #     actions = ["secretsmanager:GetSecretValue"],
+  #     resources = ["arn:aws:secretsmanager:us-east-1:0123456789012:secret:mysecert"]
+  #     effect = "Allow"
+  #   }
+  # }
+}
+
+# Helm release configurations
+
+variable "wait" {
+  description = "When true, wait until Pods are up and healthy or wait_timeout seconds before exiting terraform."
+  type        = bool
+  default     = true
+}
+
+variable "wait_timeout" {
+  description = "Number of seconds to wait for Pods to become healthy before marking the deployment as a failure."
+  type        = number
+  default     = 300
+}
+
+variable "sleep_for_resource_culling" {
+  description = "Sleep for 30 seconds to allow Kubernetes time to remove associated AWS resources."
+  type        = bool
+  default     = false
+}
+

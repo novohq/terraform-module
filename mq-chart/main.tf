0cb81077f0a9ecb18583024890498f602f76d206
Pratik Makwana <pratik@banknovo.com>
2023-01-26 01:33:21 +0530

    added rabbitmq custom chart

---
+++ mq-chart/main.tf
@@ -0,0 +1,226 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# DEPLOY HELM CHART
+# These templates allow you to deploy any Helm chart with Terraform
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+terraform {
+  # This module is now only being tested with Terraform 1.1.x. However, to make upgrading easier, we are setting 1.0.0 as the minimum version.
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      # This module is compatible with AWS provider ~> 4.6.0, but to make upgrading easier, we are setting 3.75.1 as the minimum version.
+      version = ">= 3.75.1"
+    }
+
+    helm = {
+      source = "hashicorp/helm"
+      # NOTE: 2.6.0 has a regression bug that prevents usage of the exec block with data source references, so we exclude
+      # that version. See https://github.com/hashicorp/terraform-provider-kubernetes/issues/1464 for more details.
+      version = "~> 2.0, != 2.6.0"
+    }
+  }
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# DEPLOY HELM CHART
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "helm_release" "application" {
+  name       = var.application_name
+  repository = var.helm_repository
+  chart      = var.helm_chart
+  version    = var.helm_chart_version
+  namespace  = var.namespace
+  # repository_key_file        = lookup(var.repository_key_file, null)
+  # repository_cert_file       = lookup(var.repository_cert_file, null)
+  # repository_ca_file         = lookup(var.repository_ca_file, null)
+  # repository_username        = lookup(var.repository_username, null)
+  # repository_password        = lookup(var.repository_password, null)
+  # force_update               = lookup(var.force_update, true)
+  wait                         = var.wait
+  # recreate_pods              = lookup(var.recreate_pods, true)
+  # max_history                = lookup(var.max_history, 5)
+  # lint                       = lookup(var.lint, true)
+  # cleanup_on_fail            = lookup(var.cleanup_on_fail, false)
+  # create_namespace           = lookup(var.create_namespace, false)
+  # disable_webhooks           = lookup(var.disable_webhooks, false)
+  # verify                     = lookup(var.verify, false)
+  # reuse_values               = lookup(var.reuse_values, false)
+  # reset_values               = lookup(var.reset_values, false)
+  # atomic                     = lookup(var.atomic, false)
+  # skip_crds                  = lookup(var.skip_crds, false)
+  # render_subchart_notes      = lookup(var.render_subchart_notes, true)
+  # disable_openapi_validation = lookup(var.disable_openapi_validation, false)
+  # wait_for_jobs              = lookup(var.wait_for_jobs, false)
+  # dependency_update          = lookup(var.dependency_update, false)
+  # replace                    = lookup(var.replace, false)
+  timeout                    = var.wait_timeout
+
+  values = [
+    yamlencode(
+      merge(
+        local.helm_chart_input,
+        var.override_chart_inputs,
+      ),
+    ),
+  ]
+  cleanup_on_fail = var.cleanup_on_fail
+  depends_on = [null_resource.sleep_for_resource_culling]
+}
+
+# Some charts turn Kubernetes resources from the chart into AWS resources. These are properly destroyed when the
+# corresponding Kubernetes resource is destroyed. However, because of the asynchronous nature of
+# Kubernetes operations, there is a delay before the respective controllers delete the AWS resources. This can cause
+# problems when you are destroying related resources in quick succession (e.g the Route 53 Hosted Zone).
+# We can optionally add a wait after the release is destroyed by using a time_sleep resource.
+resource "null_resource" "sleep_for_resource_culling" {
+  triggers = {
+    should_run = (
+      var.expose_type != "cluster-internal" && var.expose_type != "none"
+      ? "true"
+      : "false"
+    )
+  }
+
+  provisioner "local-exec" {
+    command = (
+      self.triggers["should_run"] == "true"
+      ? "echo 'Sleeping for 30 seconds to allow Kubernetes time to remove associated AWS resources'; sleep 30"
+      : "echo 'Skipping sleep to wait for Kubernetes to cull AWS resources, since k8s-service has none associated with it.'"
+    )
+    when = destroy
+  }
+}
+
+locals {
+  iam_role = (
+    var.iam_role_name != ""
+    ? (
+      var.iam_role_exists
+      ? data.aws_iam_role.existing_role[0].arn
+      : aws_iam_role.new_role[0].arn
+    )
+    : ""
+  )
+  helm_chart_input = merge(
+    nameOverride = var.application_name
+    serviceAccount = {
+        # Create a new service account if service_account_name is not blank and it is not referring to an existing Service
+        # Account
+        create = (!var.service_account_exists) && var.service_account_name != ""
+
+        name        = var.service_account_name
+        namespace   = var.namespace
+        annotations = local.iam_role == "" ? {} : { "eks.amazonaws.com/role-arn" = local.iam_role }
+    }
+    service = {
+        # When expose_type is cluster-internal, we do not want to associate an Ingress resource, or allow access
+        # externally from the cluster, so we use ClusterIP service type.
+        type = (
+          var.expose_type == "cluster-internal" || var.expose_type == "none"
+          ? "ClusterIP"
+          : "NodePort"
+        )
+        ports = merge(local.additional_service_ports, {
+          app = {
+            port = var.service_port
+          }
+        })
+    }
+    auth = {
+      username = var.mq_user
+      password = var.mq_password
+    }
+    resources = {
+      requests = { 
+        memory = var.mq_request_memory, 
+        cpu = var.mq_request_cpu
+      },
+      limits = { 
+        memory = var.mq_limit_memory, 
+        cpu = var.mq_limit_cpu 
+      }
+    }
+
+  )
+}
+# ---------------------------------------------------------------------------------------------------------------------
+# SET UP IAM ROLE FOR SERVICE ACCOUNT
+# Set up IRSA if a service account and IAM role are configured.
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "aws_iam_role" "new_role" {
+  count = var.iam_role_name != "" && var.iam_role_exists == false ? 1 : 0
+
+  name               = var.iam_role_name
+  assume_role_policy = module.service_account_assume_role_policy.assume_role_policy_json
+}
+
+module "service_account_assume_role_policy" {
+  source = "git::git@github.com:gruntwork-io/terraform-aws-eks.git//modules/eks-iam-role-assume-role-policy-for-service-account?ref=v0.56.1"
+
+  eks_openid_connect_provider_arn = var.eks_iam_role_for_service_accounts_config.openid_connect_provider_arn
+  eks_openid_connect_provider_url = var.eks_iam_role_for_service_accounts_config.openid_connect_provider_url
+  namespaces                      = []
+  service_accounts = [{
+    name      = var.service_account_name
+    namespace = var.namespace
+  }]
+}
+
+resource "aws_iam_role_policy" "service_policy" {
+  count = var.iam_role_name != "" && var.iam_role_exists == false && local.use_inline_policies ? 1 : 0
+
+  name   = "${var.iam_role_name}Policy"
+  role   = var.iam_role_name != "" && var.iam_role_exists == false ? aws_iam_role.new_role[0].id : data.aws_iam_role.existing_role[0].id
+  policy = data.aws_iam_policy_document.service_policy[0].json
+}
+
+resource "aws_iam_policy" "service_policy" {
+  count = var.iam_role_name != "" && var.iam_role_exists == false && var.use_managed_iam_policies ? 1 : 0
+
+  name_prefix = "${var.iam_role_name}-policy"
+  policy      = data.aws_iam_policy_document.service_policy[0].json
+}
+
+resource "aws_iam_role_policy_attachment" "service_policy" {
+  count = var.iam_role_name != "" && var.iam_role_exists == false && var.use_managed_iam_policies ? 1 : 0
+
+  role       = var.iam_role_name != "" && var.iam_role_exists == false ? aws_iam_role.new_role[0].id : data.aws_iam_role.existing_role[0].id
+  policy_arn = aws_iam_policy.service_policy[0].arn
+}
+
+data "aws_iam_policy_document" "service_policy" {
+  count = var.iam_role_name != "" ? 1 : 0
+
+  dynamic "statement" {
+    for_each = var.iam_policy == null ? {} : var.iam_policy
+
+    content {
+      sid       = statement.key
+      effect    = statement.value.effect
+      actions   = statement.value.actions
+      resources = statement.value.resources
+    }
+  }
+}
+
+data "aws_iam_role" "existing_role" {
+  count = var.iam_role_exists ? 1 : 0
+
+  name = var.iam_role_name
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# EMIT HELM CHART VALUES TO DISK FOR DEBUGGING
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "local_file" "debug_values" {
+  count = var.values_file_path != null ? 1 : 0
+
+  content         = yamlencode(local.helm_chart_input)
+  filename        = var.values_file_path
+  file_permission = "0644"
+}
---
+++ mq-chart/variables.tf
@@ -0,0 +1,191 @@
+#---------------------------------------------------------------------------------------------------------------------
+# REQUIRED MODULE PARAMETERS
+# These variables must be passed in by the operator.
+# ---------------------------------------------------------------------------------------------------------------------
+
+variable "application_name" {
+  description = "The name of the application (e.g. my-service-stage). Used for labeling Kubernetes resources."
+  type        = string
+}
+
+variable "namespace" {
+  description = "The Kubernetes Namespace to deploy the helm chart into."
+  type        = string
+}
+
+variable "helm_repository" {
+  description = "Repository URL where to locate the requested chart."
+  type        = string
+}
+
+variable "helm_chart" {
+  description = "Chart name to be installed. The chart name can be local path, a URL to a chart, or the name of the chart if repository is specified. It is also possible to use the <repository>/<chart> format here if you are running Terraform on a system that the repository has been added to with helm repo add but this is not recommended."
+  type        = string
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# OPTIONAL MODULE PARAMETERS
+# These variables have defaults, but may be overridden by the operator.
+# ---------------------------------------------------------------------------------------------------------------------
+
+variable "helm_chart_version" {
+  description = "Specify the exact chart version to install. If this is not specified, the latest version is installed."
+  type        = string
+  default     = null
+}
+
+variable "helm_chart_values" {
+  description = "Map of values to pass to the Helm chart. Leave empty to use chart default values."
+  type        = any
+  default     = {}
+}
+
+variable "iam_role_exists" {
+  description = "Whether or not the IAM role passed in `iam_role_name` already exists. Set to true if it exists, or false if it needs to be created. Defaults to false."
+  type        = bool
+  default     = false
+}
+
+variable "iam_role_name" {
+  description = "The name of an IAM role that will be used by the pod to access the AWS API. If `iam_role_exists` is set to false, this role will be created. Leave as an empty string if you do not wish to use IAM role with Service Accounts."
+  type        = string
+  default     = ""
+}
+
+variable "service_account_name" {
+  description = "The name of a service account to create for use with the Pods. This service account will be mapped to the IAM role defined in `var.iam_role_name` to give the pod permissions to access the AWS API. Must be unique in this namespace. Leave as an empty string if you do not wish to assign a Service Account to the Pods."
+  type        = string
+  default     = ""
+}
+
+variable "service_account_exists" {
+  description = "When true, and service_account_name is not blank, lookup and assign an existing ServiceAccount in the Namespace to the Pods."
+  type        = bool
+  default     = false
+}
+
+variable "eks_iam_role_for_service_accounts_config" {
+  description = "Configuration for using the IAM role with Service Accounts feature to provide permissions to the applications. This expects a map with two properties: `openid_connect_provider_arn` and `openid_connect_provider_url`. The `openid_connect_provider_arn` is the ARN of the OpenID Connect Provider for EKS to retrieve IAM credentials, while `openid_connect_provider_url` is the URL. Leave as an empty string if you do not wish to use IAM role with Service Accounts."
+  type = object({
+    openid_connect_provider_arn = string
+    openid_connect_provider_url = string
+  })
+  default = {
+    openid_connect_provider_arn = ""
+    openid_connect_provider_url = ""
+  }
+}
+
+variable "iam_policy" {
+  description = "An object defining the policy to attach to `iam_role_name` if the IAM role is going to be created. Accepts a map of objects, where the map keys are sids for IAM policy statements, and the object fields are the resources, actions, and the effect (\"Allow\" or \"Deny\") of the statement. Ignored if `iam_role_arn` is provided. Leave as null if you do not wish to use IAM role with Service Accounts."
+  type = map(object({
+    resources = list(string)
+    actions   = list(string)
+    effect    = string
+  }))
+  default = null
+
+  # Example:
+  # iam_policy = {
+  #   S3Access = {
+  #     actions = ["s3:*"]
+  #     resources = ["arn:aws:s3:::mybucket"]
+  #     effect = "Allow"
+  #   },
+  #   SecretsManagerAccess = {
+  #     actions = ["secretsmanager:GetSecretValue"],
+  #     resources = ["arn:aws:secretsmanager:us-east-1:0123456789012:secret:mysecert"]
+  #     effect = "Allow"
+  #   }
+  # }
+}
+
+# Helm release configurations
+
+variable "wait" {
+  description = "When true, wait until Pods are up and healthy or wait_timeout seconds before exiting terraform."
+  type        = bool
+  default     = true
+}
+
+variable "wait_timeout" {
+  description = "Number of seconds to wait for Pods to become healthy before marking the deployment as a failure."
+  type        = number
+  default     = 300
+}
+
+variable "sleep_for_resource_culling" {
+  description = "Sleep for 30 seconds to allow Kubernetes time to remove associated AWS resources."
+  type        = bool
+  default     = false
+}
+
+variable "cleanup_on_fail" {
+  description = "Allow deletion of new resources created in this upgrade when upgrade fails."
+  type        = bool
+  default     = null
+}
+
+variable "values_file_path" {
+  description = "A local file path where the helm chart values will be emitted. Use to debug issues with the helm chart values. Set to null to prevent creation of the file."
+  type        = string
+  default     = null
+}
+
+variable "override_chart_inputs" {
+  description = "Override any computed chart inputs with this map. This map is shallow merged to the computed chart inputs prior to passing on to the Helm Release. This is provided as a workaround while the terraform module does not support a particular input value that is exposed in the underlying chart. Please always file a GitHub issue to request exposing additional underlying input values prior to using this variable."
+
+  # Ideally we would define a concrete type here, but since the input value spec for the chart has dynamic optional
+  # values, we can't use a concrete object type for Terraform. Also, setting a type spec here will defeat the purpose of
+  # the escape hatch since it requires defining new input values here before users can use it.
+  type = any
+
+  default = {}
+}
+
+variable "use_managed_iam_policies" {
+  description = "When true, all IAM policies will be managed as dedicated policies rather than inline policies attached to the IAM roles. Dedicated managed policies are friendlier to automated policy checkers, which may scan a single resource for findings. As such, it is important to avoid inline policies when targeting compliance with various security standards."
+  type        = bool
+  default     = true
+}
+
+locals {
+  use_inline_policies = var.use_managed_iam_policies == false
+}
+
+variable "mq_user" {
+  description = "Rabbitmq user"
+  type        = string
+  default     = "rabbitmq"
+}
+
+variable "mq_password" {
+  description = "Rabbitmq password"
+  type        = string
+  default     = "rabbitmq"
+}
+
+variable "mq_request_memory" {
+  description = "Request memory"
+  type        = string
+  default     = "1000Mi"
+}
+
+variable "mq_request_cpu" {
+  description = "Request memory"
+  type        = string
+  default     = "500m"
+}
+
+variable "mq_limit_memory" {
+  description = "Limit memory"
+  type        = string
+  default     = "1000Mi"
+}
+
+variable "mq_limit_cpu" {
+  description = "Limit memory"
+  type        = string
+  default     = "500m"
+}
+
